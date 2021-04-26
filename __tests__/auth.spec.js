8710ebf452b913dea97e7899139b1a4898d78099
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-04-26 23:13:35 +0200

    TEST(#20): Add jest and first UTs for auth.js
    Install necessary modules for testing and interworking with ES6.
    Add first, experimental UTs for auth/auth.js (as merged in be5a4e4).
    Export extractTokenFromHeader to enable testing.
    Return undefined if there are no headers in request to avoid throwing
    another type of exception.

---
+++ __tests__/auth.spec.js
@@ -0,0 +1,85 @@
+import jsonwebtoken from "jsonwebtoken";
+import * as auth from "../auth/auth.js";
+
+function InvalidHeadersReq() {
+    const req = new Object();
+    req.headers = {
+        "random-header": "gibberish result",
+    };
+    return req;
+}
+
+function ValidHeadersReq() {
+    const req = new Object();
+    req.headers = {
+        authorization:
+            "UNEXPECTED\r\nUNEXPECTED\t~ż result UNEXPECTED UNEXPECTED    ",
+    };
+    return req;
+}
+
+describe("extractTokenFromHeader function", () => {
+    test("Should return undefined if input is invalid", () => {
+        expect(auth.extractTokenFromHeader(new Object())).toBeUndefined();
+        expect(
+            auth.extractTokenFromHeader(InvalidHeadersReq())
+        ).toBeUndefined();
+    });
+
+    test("Should return valid value if 'authorization' header is present", () => {
+        const result = auth.extractTokenFromHeader(ValidHeadersReq());
+
+        expect(result).toBeDefined() && expect(result).not.toBeNull();
+    });
+
+    test("Should return second word of 'authorization' header", () => {
+        const result = auth.extractTokenFromHeader(ValidHeadersReq());
+
+        expect(result).toBe("result") &&
+            expect(result).not.toMatch(/UNEXPECTED/);
+    });
+});
+
+describe("authenticateToken function", () => {
+    beforeEach(() => {
+        jsonwebtoken.verify = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("Should throw if token not present in header", () => {
+        const extractTokenFromHeaderSpy = jest.spyOn(
+            auth,
+            "extractTokenFromHeader"
+        );
+        extractTokenFromHeaderSpy.mockReturnValue(undefined);
+
+        expect(() => auth.authenticateToken(InvalidHeadersReq())).toThrow(
+            "No token."
+        );
+        expect(() => auth.authenticateToken(ValidHeadersReq())).toThrow(
+            "No token."
+        );
+
+        expect(extractTokenFromHeaderSpy).toHaveBeenCalledTimes(2);
+    });
+
+    test("Should call JSW.verify if token extracted succesfully", () => {
+        const extractTokenFromHeaderSpy = jest.spyOn(
+            auth,
+            "extractTokenFromHeader"
+        );
+        extractTokenFromHeaderSpy.mockReturnValue("token");
+
+        expect(() => auth.authenticateToken(new Object())).not.toThrow();
+
+        expect(jsonwebtoken.verify).toHaveBeenCalledWith(
+            "token",
+            process.env.ACCESS_TOKEN_SECRET,
+            expect.any(Function)
+        );
+        expect(extractTokenFromHeaderSpy).toHaveBeenCalledTimes(1);
+    });
+});

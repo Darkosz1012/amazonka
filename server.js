5e1449dc949ada6ab55368d51c687c23188fce14
Dariusz <darek10121998db@gmail.com>
2021-04-27 22:14:18 +0200

    REFACTOR(#49): restructure backend
    -separating graphql types into several files
    -splitting resolvers into several files
    -graphql link changed to http://localhost:3001/graphql
    -removing unnecessary console.log
    - adding packages fs, path, @graphql-tools/schema

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 // noinspection ES6UnusedImports
 import {} from "./config/getENV.js";
-
-import { ApolloServer } from "apollo-server";
-import fs from "fs";
-
-import resolvers from "./graphql/resolvers.js";
-
 const port = process.env.PORT || 3001;
-const typeDefs = fs.readFileSync("./graphql/schema.graphql", "utf-8");
 
-// noinspection JSCheckFunctionSignatures
-const app = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => {
-        return req;
-    },
+import express from "express";
+import graphqlServer from "./graphql/index.js";
+
+const app = express();
+
+graphqlServer.applyMiddleware({
+    app,
 });
 
 // noinspection JSIgnoredPromiseFromCall

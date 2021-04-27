948f876f60851fa89eb10cf6001c63ebc92ea7aa
Dariusz <darek10121998db@gmail.com>
2021-04-27 22:14:18 +0200

    REFACTOR(#49): restructure backend
    -separating graphql types into several files
    -splitting resolvers into several files
    -graphql link changed to http://localhost:3001/graphql
    -removing unnecessary console.log
    - adding packages fs, path, @graphql-tools/schema

---
+++ graphql/resolvers/user/index.js
@@ -0,0 +1,2 @@
+export { default as userMutations } from "./mutations.js";
+// export { default as userQueries } from './queries.js';
---
+++ graphql/resolvers/user/mutations.js
@@ -0,0 +1,52 @@
+import { User } from "../../../db/index.js";
+import { hash, verify } from "../../../auth/auth.js";
+import jwt from "jsonwebtoken";
+
+export default {
+    login: async (_, { username, password }) => {
+        const result = await User.findOne({ username })
+            .select("+password")
+            .exec();
+        if (result == null) {
+            throw {
+                message: "Username or password incorrect.",
+            };
+        }
+        if (await verify(password, result.password)) {
+            return createUserToken(result, username);
+        } else {
+            throw {
+                message: "Username or password incorrect.",
+            };
+        }
+    },
+
+    register: async (_, { username, password }) => {
+        let newUser = new User({
+            username,
+            password: await hash(password),
+        });
+        let result = await newUser.save();
+        return createUserToken(result, username);
+    },
+};
+
+function createUserToken(user, username) {
+    return {
+        accessToken: createJWT(user, "1m"),
+        refreshToken: createJWT(user, "1h"),
+        user_id: user._id,
+        username,
+    };
+}
+
+function createJWT(user, time) {
+    return jwt.sign(
+        {
+            username: user.username,
+            id: user._id,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: time }
+    );
+}

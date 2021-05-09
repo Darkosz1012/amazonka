53937b85370ba8098a309c96d69b9e2968addfa2
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-05-09 21:46:16 +0200

    REFACTOR(c07263): remove redundancy, improve names
    Test WHAT code does, not HOW - simulating button clicks is better than
    changing value of some boolean property invisible to the user.

--- front/src/components/LoginPanel/LoginPanel.test.js
+++ front/src/components/LoginPanel/LoginPanel.test.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
-import {shallow} from 'enzyme';
+import { shallow } from "enzyme";
 import LoginForm from "./comp/LoginForm";
 import RegisterForm from "./comp/RegisterForm";
 import Button from "../UI/Button/Button";
 import LoginPanel from "./LoginPanel";
 
-const wrapper = shallow(<LoginPanel />);
-it('should render <LoginPanel/>', () => {
-    expect(wrapper);
-});
-
-describe('<LoginPanel />', () => {
-
+describe("LoginPanel", () => {
     let loginPanelWrapper;
     let loginPanelInstance;
-    const loginpanel = (disableLifecycleMethods = false) => shallow(<LoginPanel />, {disableLifecycleMethods});
+    const loginpanel = (disableLifecycleMethods = false) =>
+        shallow(<LoginPanel />, { disableLifecycleMethods });
 
     beforeEach(() => {
         loginPanelWrapper = loginpanel();
@@ -24,64 +19,61 @@ describe('<LoginPanel />', () => {
     afterEach(() => {
         loginPanelWrapper = undefined;
         loginPanelInstance = undefined;
-
     });
 
-    it('renders without crashing', () => {
+    it("renders without crashing", () => {
         expect(loginpanel().exists()).toBe(true);
     });
 
-    it('renders a div', () => {
-        expect( loginPanelWrapper.first().type() ).toBe('div');
+    test("root is a div", () => {
+        expect(loginPanelWrapper.first().type()).toBe("div");
     });
 
-    describe('the rendered div', () => {
+    describe("the rendered div", () => {
         const div = () => loginPanelWrapper.first();
-    
-        it('contains everything else that gets rendered', () => {
-            expect(div().children()).toEqual(loginPanelWrapper.children())
+
+        it("contains everything else that gets rendered", () => {
+            expect(div().children()).toEqual(loginPanelWrapper.children());
         });
     });
 
-    it('renders <Button />', () => {
+    it("renders two Buttons", () => {
         expect(loginPanelWrapper.find(Button).length).toBe(2);
     });
-    
-    describe('the rendered <Button />', () => {
 
-        const button = () => loginPanelWrapper.find(Button).at(0);
-        const button2 = () => loginPanelWrapper.find(Button).at(1);
-        
-        //czy to wgl potrzebne? 
-        it('receives showLogin as "onClick" prop', () => {
-            expect(JSON.stringify(button().prop('onClick'))).toEqual(JSON.stringify(loginPanelInstance.showLogin)); 
-        })
-        it('receives showRegister as "onClick" prop', () => {
-            expect(JSON.stringify(button2().prop('onClick'))).toEqual(JSON.stringify(loginPanelInstance.showRegister)); 
-        })
-    })
+    describe("the rendered Buttons alternate between login and register forms", () => {
+        const registerButton = () => loginPanelWrapper.find(Button).at(1);
 
-    describe('when "isRegister" is false', () => {
-        beforeEach(() => {
-            loginPanelInstance.setState({isRegister: false});
-        });
-        
-        it('renders <RegisterForm />', () => { expect(loginPanelWrapper.find(RegisterForm).length).toBe(0); })     
-        it('renders <LoginForm />', () => { expect(loginPanelWrapper.find(LoginForm).length).toBe(1); })
-    });
+        describe("after clicking first button", () => {
+            const loginButton = () => loginPanelWrapper.find(Button).at(0);
 
-    describe('when "isRegister" is true', () => {
-        beforeEach(() => {
-            loginPanelInstance.setState({isRegister: true});
+            beforeEach(() => {
+                loginButton().simulate("click");
+            });
+
+            it("does not render RegisterForm", () => {
+                expect(loginPanelWrapper.find(RegisterForm).length).toBe(0);
+            });
+
+            it("renders LoginForm", () => {
+                expect(loginPanelWrapper.find(LoginForm).length).toBe(1);
+            });
         });
 
-        it('renders <RegisterForm />', () => { expect(loginPanelWrapper.find(RegisterForm).length).toBe(1); })     
-        it('renders <LoginForm />', () => { expect(loginPanelWrapper.find(LoginForm).length).toBe(0); })
-    });
+        describe("after clicking second button", () => {
+            const registerButton = () => loginPanelWrapper.find(Button).at(1);
 
+            beforeEach(() => {
+                registerButton().simulate("click");
+            });
+
+            it("renders RegisterForm", () => {
+                expect(loginPanelWrapper.find(RegisterForm).length).toBe(1);
+            });
+
+            it("does not render LoginForm", () => {
+                expect(loginPanelWrapper.find(LoginForm).length).toBe(0);
+            });
+        });
+    });
 });
-
-
-
-
-

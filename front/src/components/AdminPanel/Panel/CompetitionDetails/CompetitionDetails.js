30f59deb6fdafe467035d89cf31c63b38866e032
AleksandraRolka <arolka@interia.com>
2021-05-14 12:23:49 +0200

    FEAT(#25): add competition details component to panel

--- front/src/components/AdminPanel/Panel/CompetitionDetails/CompetitionDetails.js
+++ front/src/components/AdminPanel/Panel/CompetitionDetails/CompetitionDetails.js
@@ -1,7 +1,29 @@
-function CompetitionDetails() {
+import "./CompetitionDetails.css";
+import competitionDetaildata from "../../competitionsData.json";
+
+function CompetitionDetails(props) {
+    const _id = props.match.params.id;
+    const name = competitionDetaildata[_id - 1]["name"];
+    const location = competitionDetaildata[_id - 1]["location"];
+    const start_date = competitionDetaildata[_id - 1]["date_start"];
+    const end_date = competitionDetaildata[_id - 1]["date_end"];
     return (
         <div>
-            <p>Szczegóły zawodów</p>
+            <p className="panel-detail-header">Szczegóły zawodów</p>
+            <div className="infoDiv">
+                <div className="column left">
+                    <p id="name-label">Nazwa:</p>
+                    <p>Data rozpoczęcia:</p>
+                    <p>Data zakończenia:</p>
+                    <p>Lokalizacja:</p>
+                </div>
+                <div className="column right">
+                    <p id="name">{name}</p>
+                    <p>{start_date}</p>
+                    <p>{end_date}</p>
+                    <p>{location}</p>
+                </div>
+            </div>
         </div>
     );
 }

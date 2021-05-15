72f406bef4ccc1bda1d61ac8e173becf2fb93555
miczoc98 <miczoc98@gmail.com>
2021-05-15 13:51:26 +0200

    REFACTOR: update names in category schema

--- db/schema/CategorySchema.js
+++ db/schema/CategorySchema.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 export default new mongoose.Schema({
+    competition: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Competition",
+        required: true,
+    },
+
     name: {
         type: String,
         required: true,

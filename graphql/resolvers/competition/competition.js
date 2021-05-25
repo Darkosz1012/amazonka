0bfd2e75fcae9297b012b49400a982c356a7c0c2
miczoc98 <miczoc98@gmail.com>
2021-05-25 17:22:13 +0200

    FEAT: use data loader in competition query

--- graphql/resolvers/competition/competition.js
+++ graphql/resolvers/competition/competition.js
@@ -1,15 +1,19 @@
 import { Category, CompetitionDetails, User } from "$/db/index.js";
+import DataLoader from "$/graphql/loaders/createDefaultLoader.js";
 
 export default {
     owner: async (parent) => {
-        return User.findById(parent.owner_id);
+        const loader = new DataLoader(User);
+        return await loader.load(parent.owner_id);
     },
 
     details: async (parent) => {
-        return CompetitionDetails.findById(parent.details_id);
+        const loader = new DataLoader(CompetitionDetails);
+        return await loader.load(parent.details_id);
     },
 
     categories: async (parent) => {
-        return Category.find({ _id: { $in: parent.categories_id } });
+        const loader = new DataLoader(Category);
+        return await loader.load(parent.categories_id);
     },
 };

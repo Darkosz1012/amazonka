588020a8c4bd4f86516c2640847f848ce3e37be7
Dariusz Biela <darek10121998Db>
2021-05-28 14:58:34 +0200

    FEAT(#61): deleteCategory remove Distance, Score and Finals

--- graphql/resolvers/category/mutations.js
+++ graphql/resolvers/category/mutations.js
@@ -1,4 +1,4 @@
-import { Category, Competition} from "$/db/index.js";
+import { Category, Competition,Distance, Score, Finals} from "$/db/index.js";
 
 export default {
     addCategory: async (parent, args, context, info) => {
@@ -8,18 +8,17 @@ export default {
     },
     updateCategory: async (parent, args, context, info) => {
         let result = Category.findOneAndUpdate(
-            { _id: args.category._id },
-            { $set: args.category }
+            { _id: args._id },
+            { $set: args }
         );
-        if("competition" in args){
-            await Competition.updateOne({_id:result.competition_id},{$pull:{categories_id: result._id}});
-            await Competition.updateOne({_id:args.competition_id},{$push:{categories_id: result._id}});
-        }
-        return Category.findOne({ _id: args.category._id })
+        return Category.findOne({ _id: args._id })
     },
     deleteCategory: async (parent, args, context, info) => {
         let result = Category.findOneAndDelete({ _id: args._id });
         await Competition.updateOne({_id:result.competition_id},{$pull:{categories_id: result._id}});
+        await Distance.deleteMany({category_id:args._id});
+        await Score.deleteMany({category_id:args._id});
+        await Finals.deleteMany({category_id:args._id});
         return result;
     },
 };

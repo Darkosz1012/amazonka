139ca2b4ba83517c9f0fece46e8a2a7e878aed58
Dariusz Biela <darek10121998Db>
2021-05-28 15:01:13 +0200

    FEAT(#61): adds scores and finals for Category

--- graphql/resolvers/category/Category.js
+++ graphql/resolvers/category/Category.js
@@ -1,9 +1,18 @@
 import createDefaultLoader from "$/graphql/loaders/createDefaultLoader.js";
-import {Competition} from "$/db/index.js"
+import createLoader from "$/graphql/loaders/createLoader.js";
+import {Competition, Score, Finals} from "$/db/index.js"
 
 export default {
     competition: async (parent, args, context, info) => {
         const loader = createDefaultLoader(Competition)
         return await loader.load(parent.competition_id);
+    },
+    scores: async (parent, args, context, info) => {
+        const loader = createLoader(Score, "category_id");
+        return await loader.load(parent._id);
+    },
+    finals: async (parent, args, context, info) => {
+        const loader = createLoader(Finals, "category_id");
+        return await loader.load(parent._id);
     },
 }
--- graphql/resolvers/category/queries.js
+++ graphql/resolvers/category/queries.js
@@ -3,8 +3,8 @@ import { Category } from "$/db/index.js";
 export default {
     categories: async (parent, args, context, info) => {
         let result = [];
-        if ("competition" in args) {
-            result = await Category.find({competition: args.competition});
+        if ("competition_id" in args) {
+            result = await Category.find({competition_id: args.competition_id});
         }else{
             result = await Category.find();
         }

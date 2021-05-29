01922fede729b0ba1c1555c87bf83c1cf2dc193c
miczoc98 <miczoc98@gmail.com>
2021-05-29 20:01:44 +0200

    REFACTOR: update tests

--- __tests__/unit/graphql/resolvers/user/mutations.test.js
+++ __tests__/unit/graphql/resolvers/user/mutations.test.js
@@ -143,7 +143,7 @@ describe("register function", () => {
 
         it("should throw if username is not ascii", async () => {
             let userData = createUserData();
-            userData.username = "😸";
+            userData.username = "🦝";
 
             await expect(
                 mutations.register(undefined, userData)
